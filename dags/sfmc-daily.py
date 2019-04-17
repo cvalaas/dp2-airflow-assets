052237dbec7f46d800b29f1463582ea818303938
Chris Valaas <cvalaas@mozilla.com>
2019-04-16 21:40:26 -0700

    removing config mounts and adding variables

--- dags/sfmc-daily.py
+++ dags/sfmc-daily.py
@@ -5,32 +5,35 @@ from airflow.contrib.kubernetes import pod
 from airflow.contrib.kubernetes import secret, volume, volume_mount
 from airflow.contrib.operators import kubernetes_pod_operator
 from airflow.operators.bash_operator import BashOperator
+from airflow.models import Variable
+
+#PROJECT=os.environ.get('gcp_project')
+
+PROJECT=Variable.get('gcp_project')
+LATEST_TAG=Variable.get('latest_di_image_tag')
 
 # TODO: put the latest SHA hash in a bucket somewhere and pull from there
-IMAGE='gcr.io/imposing-union-227917/data-integrations:20190307'
+IMAGE='gcr.io/' + PROJECT + '/data-integrations:' + LATEST_TAG
+
+# TODO:
+#   1. Create a new data-intgrations image, upload it, and put the hash here
+#   2. Add the salesforce secrets to the di-secrets.yaml file and push to k8s
+#   3. Add the secrets_salesforceftp.py to the configmap and upload to k8s
+#   4. Confirm Sparkjob can accept "DailyEmailSendSummary*" as a filename
+#   5. Change sparkjob to handle this file (including the fact that it's CSV not TSV!)
 
 brick_secret = secret.Secret( deploy_type='env',
     deploy_target='BRICKFTP_API_KEY',
     secret='di-secrets',
     key='BRICKFTP_API_KEY')
-
-config_volume = volume.Volume(
-            name = 'configs',
-            configs = {
-              'configMap': {
-                'name': 'data-integrations-dev',
-                'items': [
-                  { 'key': 'secrets_brickftp.py', 'path': 'secrets_brickftp.py', },
-                  { 'key': 'secrets_util.py',     'path': 'secrets_util.py',     },
-                ],
-              },
-            }
-         )
-config_mount = volume_mount.VolumeMount(
-    name = 'configs',
-    mount_path = '/configs',
-    sub_path=None,
-    read_only = True)
+sf_ftp_user = secret.Secret( deploy_type='env',
+    deploy_target='SALESFORCE_FTP_USERNAME',
+    secret='di-secrets',
+    key='SALESFORCE_FTP_USERNAME')
+sf_ftp_pass = secret.Secret( deploy_type='env',
+    deploy_target='SALESFORCE_FTP_PASSWORD',
+    secret='di-secrets',
+    key='SALESFORCE_FTP_PASSWORD')
 
 default_args = {
           'retries': 5,
@@ -57,6 +60,16 @@ sfmc_fetch = kubernetes_pod_operator.KubernetesPodOperator(
         #cmds=['sh', '-c', '/usr/local/bin/brickftp_poc.py --dest-dir /tmp/ --date {{ macros.ds_add(ds, -1) }} && gsutil cp /tmp/{{ macros.ds_add(ds, -1) }} gs://moz-it-data-dp2-incoming-dev/sfmc/'],
         cmds=['sh', '-c', '/usr/local/bin/brickftp_poc.py --dest-dir /tmp/ --date {{ ds }} && gsutil cp -r /tmp/{{ ds }} gs://moz-it-data-dp2-incoming-dev/sfmc/'],
         #cmds=['sh', '-c', 'sleep 360'],
+        dag=dag)
+
+sf_ftp_fetch = kubernetes_pod_operator.KubernetesPodOperator(
+        task_id='sf-ftp-daily-fetcher',
+        name='sf-ftp-daily-fetcher',
+        namespace='default',
+        image=IMAGE,
+        secrets=[sf_ftp_user, sf_ftp_pass],
+        cmds=['sh', '-c', '/usr/local/bin/salesforce_ftp.py --dest-dir /tmp/ --date {{ ds }} && gsutil cp -r /tmp/{{ ds }} gs://moz-it-data-dp2-incoming-dev/sfmc/'],
+        #cmds=['sh', '-c', 'sleep 360'],
         volumes=[config_volume],
         volume_mounts=[config_mount],
         dag=dag)
@@ -77,3 +90,10 @@ for filename in ['Bounces','Clicks','Opens','SendJobs','Sent','Subscribers','Uns
   bash_op[filename].set_upstream(sfmc_fetch)
   bash_op[filename].set_downstream(cleanup_op)
 
+process_dess = BashOperator(
+                  task_id='sfmc-daily-load-DESS',
+                  bash_command='gcloud dataproc jobs submit pyspark gs://moz-it-data-dp2-sparkjobs-dev/etl/sfmc_loader.py --cluster=etl-cluster --region us-central1 -- gs://moz-it-data-dp2-incoming-dev/sfmc/{{ ds }} DailyEmailSendSummary* gs://moz-it-data-dp2-sfmc-dev',
+                  dag=dag)
+
+process_dess.set_upstream(sf_ftp_fetch)
+process_dess.set_downstream(cleanup_op)
