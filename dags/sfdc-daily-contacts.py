b1d875bf9fbc12569b9f9154f652f79fc630a0a5
Chris Valaas <cvalaas@mozilla.com>
2019-04-20 21:52:12 -0700

    switching from spark to bq

--- dags/sfdc-daily-contacts.py
+++ dags/sfdc-daily-contacts.py
@@ -8,15 +8,16 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.models import Variable
 
 
-#PROJECT          = Variable.get('gcp_project')
 PROJECT          = os.environ.get('gcp_project')
-LATEST_TAG       = Variable.get('latest_di_image_tag')
+LATEST_DI_TAG    = Variable.get('latest_di_image_tag')
+LATEST_BQ_TAG    = Variable.get('latest_queries_tag')
 INCOMING_BUCKET  = PROJECT + '-data-incoming'
 SPARKJOBS_BUCKET = PROJECT + '-data-sparkjobs'
 OUTPUT_BUCKET    = PROJECT + '-data-sfdc'
+QUERIES_BUCKET   = PROJECT + '-data-etl-queries'
 
-# TODO: put the latest SHA hash in a bucket somewhere and pull from there
-IMAGE='gcr.io/' + PROJECT + '/data-integrations:' + LATEST_TAG
+DI_IMAGE         = 'gcr.io/' + PROJECT + '/data-integrations:' + LATEST_DI_TAG
+QUERIES_IMAGE    = 'gcr.io/' + PROJECT + '/bigquery-queries:' + LATEST_BQ_TAG
 
 sf_secret = secret.Secret( deploy_type='volume',
     deploy_target='/config',
@@ -29,7 +30,7 @@ default_args = {
           'retry_exponential_backoff': True,
           'max_retry_delay': datetime.timedelta(minutes=10),
           #'end_date': datetime.datetime(2019,2,19),
-          'start_date': datetime.datetime(2019,4,17),
+          'start_date': datetime.datetime(2019,4,19),
           'catchup_by_default': False,
           # TODO: email stuff
 }
@@ -40,9 +41,10 @@ dag = DAG(dag_id='sfdc-daily-contacts',
           default_args=default_args
          )
 
-fetch_op = {}
-clean_op = {}
-bash_op  = {}
+fetch_op   = {}
+clean_op   = {}
+load_op    = {}
+queries_op = {}
 
 for report in ['contacts']:
 
@@ -53,24 +55,48 @@ for report in ['contacts']:
         task_id='sfdc-daily-fetcher-' + report_name_for_k8s,
         name='sfdc-daily-fetcher-'+ report_name_for_k8s,
         namespace='default',
-        image=IMAGE,
+        image=DI_IMAGE,
         secrets=[sf_secret],
         cmds=['sh', '-c', 'export LC_ALL=C.UTF-8; export LANG=C.UTF-8; /usr/local/bin/salesforce-fetcher --config-file /config/salesforce_fetcher_config.yaml --fetch-only ' + report + ' && gsutil cp -r /tmp/salesforce_fetcher/* gs://'+INCOMING_BUCKET+'/sfdc/'],
         #cmds=['sh', '-c', 'sleep 3600'],
+        dag=dag) 
+
+# arrrggghhh, the fetch op cannot run historically (since the query only pulls current data),
+# and then bad data will be loaded into the SUMMARY table.
+# Decouple the fetch op from the rest of the ops? Make it a separate thingy with no catchup?
+
+  load_op[report] = BashOperator(
+                  task_id='sfdc-daily-load-' + report_name_for_k8s,
+                  bash_command='bq load --location=US --source_format CSV --skip_leading_rows=1 ' +  \
+                               '--autodetect --replace sfdc.'+ report + ' gs://' + INCOMING_BUCKET + \
+                               '/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }}/output.csv',
+                  dag=dag)
+
+  load_op[report].set_upstream(fetch_op[report])
+
+  queries_op[report] = kubernetes_pod_operator.KubernetesPodOperator(
+        task_id='sfdc-daily-queries-' + report_name_for_k8s,
+        name='sfdc-daily-queries-' + report_name_for_k8s,
+        namespace='default',
+        image=QUERIES_IMAGE,
+        cmds=['bash', '-c', '/usr/local/bin/sfdc_contacts_queries.sh {{ macros.ds_add(ds, 1) }} '+QUERIES_BUCKET],
+        #cmds=['sh', '-c', 'sleep 3600'],
         dag=dag)
+
+  queries_op[report].set_upstream(load_op[report])
 
   # The fetcher seems to report failure even when everything succeeds. Perhaps because airflow loses
   # its connection to the Pod? Shrug. Anyway, setting the trigger_rule for the next task to 'all_done'
   # will cause it to run even if the fetcher reports "failure"
   # Ah, known issue: https://cloud.google.com/composer/docs/how-to/using/using-kubernetes-pod-operator#task_fails_despite_pod_success
-
-  bash_op[report] = BashOperator(
-                  trigger_rule='all_done',
-                  task_id='sfdc-daily-load-' + report_name_for_k8s,
-                  bash_command='gcloud dataproc jobs submit pyspark gs://'+SPARKJOBS_BUCKET+'/etl/sfdc_loader.py --cluster=etl-cluster --region us-central1 -- gs://'+INCOMING_BUCKET+'/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }} ' + report + ' gs://'+OUTPUT_BUCKET,
-                  dag=dag)
-
-  bash_op[report].set_upstream(fetch_op[report])
+  #
+  #  bash_op[report] = BashOperator(
+  #                  trigger_rule='all_done',
+  #                  task_id='sfdc-daily-load-' + report_name_for_k8s,
+  #                  bash_command='gcloud dataproc jobs submit pyspark gs://'+SPARKJOBS_BUCKET+'/etl/sfdc_loader.py --cluster=etl-cluster --region us-central1 -- gs://'+INCOMING_BUCKET+'/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }} ' + report + ' gs://'+OUTPUT_BUCKET,
+  #                  dag=dag)
+  #
+  #  bash_op[report].set_upstream(fetch_op[report])
 
   # Do we need to cleanup? No PII here.
   clean_op[report] = BashOperator(
@@ -79,92 +105,5 @@ for report in ['contacts']:
                bash_command='echo "NOOP"',
                dag=dag)
 
-  clean_op[report].set_upstream(bash_op[report])
+  clean_op[report].set_upstream(load_op[report])
 
-
-query = "DELETE FROM sfdc.summary WHERE rollup_name NOT LIKE '%Unsubscribes' AND date=CURRENT_DATE()"
-delete_query_unsub = BashOperator(
-                        task_id='sfdc-daily-bq-delete-unsubscribes',
-                        bash_command='bq query --use_legacy_sql=FALSE "%s"' % query,
-                        dag=dag)
-delete_query_unsub.set_upstream(bash_op['contacts'])
-
-query = "DELETE FROM sfdc.summary WHERE rollup_name LIKE '%Unsubscribes' AND date=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY)"
-delete_query_sub = BashOperator(
-                        task_id='sfdc-daily-bq-delete-subscribes',
-                        bash_command='bq query --use_legacy_sql=FALSE "%s"' % query,
-                        dag=dag)
-delete_query_sub.set_upstream(bash_op['contacts'])
-
-#
-# Now insert into summary table
-# I do not like this one bit.
-#
-table_name    = "sfdc.summary"
-insert_fields = "rollup_name, rollup_value, date, mailing_country, email_language, email_format"
-select_fields = "snapshot_date, MailingCountry, Email_Language__c, Email_Format__c"
-addtl_where   = "AND Double_Opt_In__c = TRUE AND HasOptedOutOfEmail = FALSE AND Subscriber__c = TRUE"
-
-summary_subscribe_queries = {
-  'unique-contacts': "INSERT INTO %s (%s) SELECT 'Unique Contacts', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() GROUP BY %s" % (table_name, insert_fields, select_fields, select_fields),
-
-  'opted-in': "INSERT INTO %s (%s) SELECT 'Opted In', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() %s AND Subscriber__c = TRUE GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-
-  'opted-out': "INSERT INTO %s (%s) SELECT 'Opted Out', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() AND (Double_Opt_In__c = FALSE OR HasOptedOutOfEmail = TRUE OR Subscriber__c = FALSE) GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-
-  'moz-sub': "INSERT INTO %s (%s) SELECT 'Mozilla Subscriber', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() %s AND moz_subscriber=TRUE GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-
-  'dev-sub': "INSERT INTO %s (%s) SELECT 'Developer Subscriber', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() %s AND dev_subscriber=TRUE GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-
-  'fx-sub': "INSERT INTO %s (%s) SELECT 'Firefox Subscriber', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() %s AND fx_subscriber=TRUE GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-
-  'other-sub': "INSERT INTO %s (%s) SELECT 'Other Subscriber', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() %s AND other_subscriber=TRUE GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-
-  'moz-labs-sub': "INSERT INTO %s (%s) SELECT 'Mozilla Labs Subscriber', COUNT(*), %s FROM sfdc.contacts_vw WHERE snapshot_date=CURRENT_DATE() %s AND moz_labs_subscriber=TRUE GROUP BY %s" % (table_name, insert_fields, select_fields, addtl_where, select_fields),
-}
-
-select_fields = "DATE(contact_history_vw.CreatedDate), MailingCountry, Email_Language__c, Email_Format__c"
-
-summary_unsubscribe_queries = {
-  'moz-unsubs': "INSERT INTO %s (%s) SELECT 'Mozilla Unsubscribes', COUNT(*), %s FROM sfdc.contact_history_vw LEFT JOIN sfdc.contacts_vw ON (ContactId=Id) WHERE DATE(contact_history_vw.CreatedDate)=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND contacts_vw.snapshot_date=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND LOWER(OldValue)='true' AND LOWER(NewValue)='false' AND Field='Mozilla' GROUP BY %s" % (table_name, insert_fields, select_fields, select_fields),
-
-  'fx-unsubs': "INSERT INTO %s (%s) SELECT 'Firefox Unsubscribes', COUNT(*), %s FROM sfdc.contact_history_vw LEFT JOIN sfdc.contacts_vw ON (ContactId=Id) WHERE DATE(contact_history_vw.CreatedDate)=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND contacts_vw.snapshot_date=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND LOWER(OldValue)='true' AND LOWER(NewValue)='false' AND Field='Firefox' GROUP BY %s" % (table_name, insert_fields, select_fields, select_fields),
-
-  'other-unsubs': "INSERT INTO %s (%s) SELECT 'Other Unsubscribes', COUNT(*), %s FROM sfdc.contact_history_vw LEFT JOIN sfdc.contacts_vw ON (ContactId=Id) WHERE DATE(contact_history_vw.CreatedDate)=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND contacts_vw.snapshot_date=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND LOWER(OldValue)='true' AND LOWER(NewValue)='false' AND Field='Other' GROUP BY %s" % (table_name, insert_fields, select_fields, select_fields),
-
-  'dev-unsubs': "INSERT INTO %s (%s) SELECT 'Developer Unsubscribes', COUNT(*), %s FROM sfdc.contact_history_vw LEFT JOIN sfdc.contacts_vw ON (ContactId=Id) WHERE DATE(contact_history_vw.CreatedDate)=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND contacts_vw.snapshot_date=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND LOWER(OldValue)='true' AND LOWER(NewValue)='false' AND Field='Developer' GROUP BY %s" % (table_name, insert_fields, select_fields, select_fields),
-
-  'moz-labs-unsubs': "INSERT INTO %s (%s) SELECT 'Mozilla Labs Unsubscribes', COUNT(*), %s FROM sfdc.contact_history_vw LEFT JOIN sfdc.contacts_vw ON (ContactId=Id) WHERE DATE(contact_history_vw.CreatedDate)=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND contacts_vw.snapshot_date=DATE_SUB( CURRENT_DATE(), INTERVAL 1 DAY) AND LOWER(OldValue)='true' AND LOWER(NewValue)='false' AND Field='Mozilla_Labs' GROUP BY %s" % (table_name, insert_fields, select_fields, select_fields),
-}
-
-sub_ops = {}
-previous_job = None
-for query_name in summary_subscribe_queries:
-  sub_ops[query_name] = BashOperator(
-                        task_id='sfdc-daily-bq-insert-' + query_name,
-                        bash_command='bq query --use_legacy_sql=FALSE "%s"' % summary_subscribe_queries[query_name],
-                        dag=dag)
-  #
-  # make each job dependent on the last so they don't all run at once,
-  # otherwise bigquery has a sad
-  #
-  if previous_job:
-    sub_ops[query_name].set_upstream(previous_job)
-  else:
-    sub_ops[query_name].set_upstream(delete_query_sub)
-
-  previous_job = sub_ops[query_name]
-
-unsub_ops = {}
-previous_job = None
-for query_name in summary_unsubscribe_queries:
-  unsub_ops[query_name] = BashOperator(
-                        task_id='sfdc-daily-bq-insert-' + query_name,
-                        bash_command='bq query --use_legacy_sql=FALSE "%s"' % summary_unsubscribe_queries[query_name],
-                        dag=dag)
-  if previous_job:
-    unsub_ops[query_name].set_upstream(previous_job)
-  else:
-    unsub_ops[query_name].set_upstream(delete_query_unsub)
-
-  previous_job = unsub_ops[query_name]
