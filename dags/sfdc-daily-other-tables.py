1c60035ada440d94349513f5a2d25459ceb52c79
Chris Valaas <cvalaas@mozilla.com>
2019-04-20 21:53:40 -0700

    switching from spark to bq

--- dags/sfdc-daily-other-tables.py
+++ dags/sfdc-daily-other-tables.py
@@ -30,7 +30,7 @@ default_args = {
           'retry_exponential_backoff': True,
           'max_retry_delay': datetime.timedelta(minutes=10),
           #'end_date': datetime.datetime(2019,2,19),
-          'start_date': datetime.datetime(2019,2,26),
+          'start_date': datetime.datetime(2019,4,18),
           'catchup_by_default': False,
           # TODO: email stuff
 }
@@ -43,9 +43,10 @@ dag = DAG(dag_id='sfdc-daily-other-tables',
 
 fetch_op = {}
 clean_op = {}
-bash_op  = {}
+load_op  = {}
+merge_op = {}
 
-for report in ['contact_donor_count','contact_history','donation_record_count','foundation_signups','petition_signups']:
+for report in ['contact_donor_count','donation_record_count','foundation_signups','petition_signups']:
 
   # stupid airflow not sanitizing input
   report_name_for_k8s = re.sub('_', '-', report)
@@ -60,18 +61,89 @@ for report in ['contact_donor_count','contact_history','donation_record_count','
         #cmds=['sh', '-c', 'sleep 3600'],
         dag=dag)
 
-  bash_op[report] = BashOperator(
+  load_op[report] = BashOperator(
                   task_id='sfdc-daily-load-' + report_name_for_k8s,
-                  bash_command='gcloud dataproc jobs submit pyspark gs://'+SPARKJOBS_BUCKET+'/etl/sfdc_loader.py --cluster=etl-cluster --region us-central1 -- gs://'+INCOMING_BUCKET+'/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }} ' + report + ' gs://'+OUTPUT_BUCKET',
+                  bash_command='bq load --location=US --source_format CSV --skip_leading_rows=1 --autodetect --replace sfdc.'+ report + ' gs://' + INCOMING_BUCKET + '/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }}/output.csv',
                   dag=dag)
 
-  bash_op[report].set_upstream(fetch_op[report])
+  load_op[report].set_upstream(fetch_op[report])
 
-  # Do we need to cleanup? No PII here.
-  clean_op[report] = BashOperator(
-               task_id='sfdc-daily-load-cleanup-' + report_name_for_k8s,
-               #bash_command='gsutil rm gs://'+INCOMING_BUCKET+'/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }}',
-               bash_command='echo "NOOP"',
-               dag=dag)
+#  # Do we need to cleanup? No PII here.
+#  clean_op[report] = BashOperator(
+#               task_id='sfdc-daily-load-cleanup-' + report_name_for_k8s,
+#               #bash_command='gsutil rm gs://'+INCOMING_BUCKET+'/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }}',
+#               bash_command='echo "NOOP"',
+#               dag=dag)
+#
+#  clean_op[report].set_upstream(load_op[report])
 
-  clean_op[report].set_upstream(bash_op[report])
+#
+# contact_history is a date-based query, so shouldn't be overwritten daily
+# BUT, the query specifies "YESTERDAY", so we can't run the fetcher on 
+# historical data, either.
+#
+for report in ['contact_history']:
+  # stupid airflow not sanitizing input
+  report_name_for_k8s = re.sub('_', '-', report)
+
+  fetch_op[report] = kubernetes_pod_operator.KubernetesPodOperator(
+        task_id='sfdc-daily-fetcher-' + report_name_for_k8s,
+        name='sfdc-daily-fetcher-'+ report_name_for_k8s,
+        namespace='default',
+        image=IMAGE,
+        secrets=[sf_secret],
+        cmds=['sh', '-c', 'export LC_ALL=C.UTF-8; export LANG=C.UTF-8; /usr/local/bin/salesforce-fetcher --config-file /config/salesforce_fetcher_config.yaml --fetch-only ' + report + ' && gsutil cp -r /tmp/salesforce_fetcher/* gs://'+INCOMING_BUCKET+'/sfdc/'],
+        #cmds=['sh', '-c', 'sleep 3600'],
+        dag=dag)
+
+  load_op[report] = BashOperator(
+                  task_id='sfdc-daily-load-' + report_name_for_k8s,
+                  bash_command='bq load --location=US --source_format CSV --skip_leading_rows=1 --replace sfdc_staging.'+ report + ' gs://' + INCOMING_BUCKET + '/sfdc/' + report + '/{{ macros.ds_add(ds, 1) }}/output.csv',
+                  dag=dag)
+
+  load_op[report].set_upstream(fetch_op[report])
+
+  merge_query = ''
+  if report == 'contact_history':
+    merge_query = """
+      MERGE sfdc.contact_history P
+      USING sfdc_staging.contact_history S
+      ON P.CreatedDate=PARSE_DATETIME('%Y-%m-%dT%T.000+0000', S.CreatedDate)
+        AND P.Field=S.Field AND P.ContactId=S.ContactId
+      WHEN NOT MATCHED THEN
+        INSERT (Field,ContactId,CreatedDate,NewValue,OldValue)
+        VALUES (Field,ContactId,PARSE_DATETIME('%Y-%m-%dT%T.000+0000', CreatedDate),NewValue,OldValue);
+    """
+
+  merge_query = re.sub("\n| {2,}", ' ', merge_query)
+  if re.search('"', merge_query):
+    raise Exception("You must not use double quotes in your query!")
+
+  merge_op[report] = BashOperator(
+                  task_id='sfdc-daily-merge-' + report_name_for_k8s,
+                  bash_command='bq query --use_legacy_sql=FALSE "%s"' % merge_query,
+                  dag=dag)
+
+  merge_op[report].set_upstream(load_op[report])
+
+
+#
+#  del_op[report] = BashOperator(
+#                  task_id='sfdc-daily-delete-' + report_name_for_k8s,
+#                  bash_command='bq query "DELETE FROM sfdc.' + report + ' WHERE snapshot_date={{ macros.ds_add(ds, 1) }}"',
+#                  dag=dag)
+#
+#  del_op[report].set_upstream(load_op[report])
+#
+#INSERT INTO sfdc.contact_donor_count
+#(Opportunity_Name, Amount , Contact_ID, snapshot_date)
+#SELECT Opportunity_Name, Amount , Contact_ID, {{ macros.ds_add(ds, 1) }}
+#FROM sfdc_staging.contact_donor_count;
+#
+#  ins_op[report] = BashOperator(
+#                  task_id='sfdc-daily-insert-' + report_name_for_k8s,
+#                  bash_command='bq query "dc.' + report + ' WHERE snapshot_date={{ macros.ds_add(ds, 2) }}"',
+#                  dag=dag)
+#
+#  ins_op[report].set_upstream(del_op[report])
+
